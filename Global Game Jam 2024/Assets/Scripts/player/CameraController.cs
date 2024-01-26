e7bf23ff9a0db2d8c8ca2d893234e6e6a77f81f6
Niek Melet <niek+github@meletstudios.com>
2024-01-26 18:57:09 +0100

    Added Cinemachine cam movement

--- Global Game Jam 2024/Assets/Scripts/player/CameraController.cs
+++ Global Game Jam 2024/Assets/Scripts/player/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform playerPos;
+    private Vector3 desiredPosition;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float camSpeed;
-
+    
     private void Start()
     {
         playerPos = FindObjectOfType<Player>().transform;
@@ -15,9 +17,13 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        desiredPosition = playerPos.position + offset;
+    }
+
+    private void LateUpdate()
+    {
         Vector3 camPos = transform.position;
         
-        Vector3 desiredPosition = playerPos.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(camPos, desiredPosition, camSpeed * Time.deltaTime);
         
         transform.position = smoothedPosition;
--- Global Game Jam 2024/Assets/Scripts/player/Player.cs
+++ Global Game Jam 2024/Assets/Scripts/player/Player.cs
@@ -1,15 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : Monkey
 {
-    private void Update()
+    private void Start()
     {
-        Vector2 movement = Move();
-        transform.Translate(movement * (Speed * Time.deltaTime));
+        monkeyRb = GetComponent<Rigidbody2D>(); 
     }
-    
+
+    private void FixedUpdate()
+    {
+        // Get direction from player input
+        Vector2 direction = Move();
+
+        if (direction.magnitude > 0)
+        {
+            // If direction is not zero (player is pressing keys), add force to move
+            monkeyRb.AddForce(direction * speed);
+        }
+        else
+        {
+            // if player is not pressing any keys, make sure to stop the player after a bit
+            monkeyRb.velocity = Vector2.Lerp(monkeyRb.velocity, Vector2.zero, 0.1f);
+        }
+
+        // Clamp the velocity to make sure the player doesn't go too fast
+        monkeyRb.velocity = Vector2.ClampMagnitude(monkeyRb.velocity, speed);
+    }
+
     protected override void Attack()
     {
         base.Attack();
