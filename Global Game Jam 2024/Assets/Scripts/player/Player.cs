8ea9d85dba41949f177a739147bd6d28591a0eaf
Niek Melet <niek+github@meletstudios.com>
2024-01-26 19:00:01 +0100

    Player Movement Done

--- Global Game Jam 2024/Assets/Scripts/player/Player.cs
+++ Global Game Jam 2024/Assets/Scripts/player/Player.cs
@@ -8,25 +8,18 @@ public class Player : Monkey
 {
     private void Start()
     {
-        monkeyRb = GetComponent<Rigidbody2D>(); 
+        monkeyRb = GetComponent<Rigidbody2D>();
+        monkeyRb.drag = 5f;
     }
 
     private void FixedUpdate()
     {
         // Get direction from player input
         Vector2 direction = Move();
-
-        if (direction.magnitude > 0)
-        {
-            // If direction is not zero (player is pressing keys), add force to move
-            monkeyRb.AddForce(direction * speed);
-        }
-        else
-        {
-            // if player is not pressing any keys, make sure to stop the player after a bit
-            monkeyRb.velocity = Vector2.Lerp(monkeyRb.velocity, Vector2.zero, 0.1f);
-        }
-
+        
+        // If direction is not zero (player is pressing keys), add force to move
+        monkeyRb.AddForce(direction * speed);
+        
         // Clamp the velocity to make sure the player doesn't go too fast
         monkeyRb.velocity = Vector2.ClampMagnitude(monkeyRb.velocity, speed);
     }
