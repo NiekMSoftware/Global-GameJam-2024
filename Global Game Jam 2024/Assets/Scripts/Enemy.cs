8a145b3aec32382a52839e21b13b97dba733f7d1
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-01-26 20:22:45 +0100

    My scene, my script, some package

---
+++ Global Game Jam 2024/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Monkey
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void Attack()
+    {
+
+    }
+
+    protected override Vector2 Move()
+    {
+        return Vector2.zero;
+    }
+}
