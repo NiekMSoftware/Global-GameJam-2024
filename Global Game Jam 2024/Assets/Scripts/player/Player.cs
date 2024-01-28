b676b9a36d9fef13f42f5eef5ce72eb3482eeb42
Doggo499 <84175344+Doggo499@users.noreply.github.com>
2024-01-28 09:17:45 +0100

    yes

--- Global Game Jam 2024/Assets/Scripts/player/Player.cs
+++ Global Game Jam 2024/Assets/Scripts/player/Player.cs
@@ -20,7 +20,6 @@ public class Player : Monkey
     [SerializeField] AudioSource audio;
     float audioCd;
 
-    private bool immune = false;
     public bool immune = false;
     public bool stunned = false;
 
